d6803c9db41f6848eaaec16c6a65f4ea6edb633c
kasansinzen <55124358+kasansinzen@users.noreply.github.com>
2022-03-05 04:12:20 +0700

    create code for section 8

--- components/ui/ErrorAlert.tsx
+++
@@ -1,7 +0,0 @@
-import classes from './ErrorAlert.module.css';
-
-const ErrorAlert: React.FC = (props) => {
-  return <div className={classes.alert}>{props.children}</div>;
-}
-
-export default ErrorAlert;
